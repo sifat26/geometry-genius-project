4cfc685fa225ab71cc5e4b044da5ac66330ce764
sifat26 <sifatict26@gmail.com>
2024-02-15 22:38:42 +0600

    Functionality added

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -8,6 +8,24 @@ function triangleCalculate(){
     const triangleHEight=triangleHeightInput.value;
     const height=parseFloat(triangleHEight);
     //console.log(height);
+    const Area= 0.5* base* height;
+    console.log(Area);
+    const setArea=document.getElementById("triangle-area");
+    setArea.innerHTML=Area;
 
 
+}
+function rectangleCalculate(){
+    const rectangleBaseInput=document.getElementById("rectangle-base");
+    const rectangleBase=rectangleBaseInput.value;
+    const base=parseFloat(rectangleBase);
+    //console.log(base);
+    const rectangleHeightInput=document.getElementById("rectangle-height");
+    const rectangleHEight=rectangleHeightInput.value;
+    const height=parseFloat(rectangleHEight);
+    //console.log(height);
+    const Area= base* height;
+    console.log(Area);
+    const setArea=document.getElementById("rectangle-area");
+    setArea.innerHTML=Area;
 }
