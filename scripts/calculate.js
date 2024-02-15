24e7096c3195d5e4d79fa09285eb6d53861fd898
sifat26 <sifatict26@gmail.com>
2024-02-15 15:39:58 +0600

    first commit

---
+++ scripts/calculate.js
@@ -0,0 +1,13 @@
+function triangleCalculate(){
+   
+    const triangleBaseInput=document.getElementById("triangle-base");
+    const triangleBase=triangleBaseInput.value;
+    const base=parseFloat(triangleBase);
+    //console.log(base);
+    const triangleHeightInput=document.getElementById("triangle-height");
+    const triangleHEight=triangleHeightInput.value;
+    const height=parseFloat(triangleHEight);
+    //console.log(height);
+
+
+}
